98383957307c58aed9b1fcad44c5a8e86139b24b
premierboy <joshua.mwangangi@students.jkuat.ac.ke>
2020-09-17 06:32:03 +0300

    Updates

---
+++ precache-manifest.6984476b992297a72bc79d5aab6015bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d221d7610f4432cb614ff807af8614c5",
+    "url": "/joshuandeleva/covid19-react-track/index.html"
+  },
+  {
+    "revision": "57c27b65ea2b2eb31b32",
+    "url": "/joshuandeleva/covid19-react-track/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "6042590e2d82338b4d6d",
+    "url": "/joshuandeleva/covid19-react-track/static/css/main.aaeb224a.chunk.css"
+  },
+  {
+    "revision": "57c27b65ea2b2eb31b32",
+    "url": "/joshuandeleva/covid19-react-track/static/js/2.462fda4f.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/joshuandeleva/covid19-react-track/static/js/2.462fda4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6042590e2d82338b4d6d",
+    "url": "/joshuandeleva/covid19-react-track/static/js/main.125c0cff.chunk.js"
+  },
+  {
+    "revision": "7d6840319390b0b20c60",
+    "url": "/joshuandeleva/covid19-react-track/static/js/runtime-main.603b0829.js"
+  }
+]);
